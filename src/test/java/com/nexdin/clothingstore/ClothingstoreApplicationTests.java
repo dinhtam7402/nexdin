6b11369afa8162ef88abdfda94d1ec4e76b51469
Nguyen Dinh Tam <tamnd7402@gmail.com>
2025-02-14 21:26:53 +0700

    project launch

---
+++ src/test/java/com/nexdin/clothingstore/ClothingstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexdin.clothingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
